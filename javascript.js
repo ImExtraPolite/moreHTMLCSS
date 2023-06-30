bd370a2d2785e65c352ae83a87972e7f78540f72
ImExtraPolite <jaleel1666@gmail.com>
2023-06-30 19:10:25 -0400

    done

--- javascript.js
+++ javascript.js
@@ -1,23 +1,30 @@
+// Modal 1
 const modal1 = document.querySelector(".modal1");
 const openModal1 = document.querySelector(".openModal1");
 const closeModal1 = document.querySelector(".closeModal1");
+// Modal 2
 const modal2 = document.querySelector(".modal2");
 const openModal2 = document.querySelector(".openModal2");
 const closeModal2 = document.querySelector(".closeModal2");
+// Modal 3
 const modal3 = document.querySelector(".modal3");
 const openModal3 = document.querySelector(".openModal3");
 const closeModal3 = document.querySelector(".closeModal3");
+// Modal 4
 const modal4 = document.querySelector(".modal4");
 const openModal4 = document.querySelector(".openModal4");
 const closeModal4 = document.querySelector(".closeModal4");
+// Modal 5
 const modal5 = document.querySelector(".modal5");
 const openModal5 = document.querySelector(".openModal5");
 const closeModal5 = document.querySelector(".closeModal5");
+// Modal 6
 const modal6 = document.querySelector(".modal6");
 const openModal6 = document.querySelector(".openModal6");
 const closeModal6 = document.querySelector(".closeModal6");
 
 
+// Modal 1
 openModal1.addEventListener("click", () => {
     modal1.showModal();
 });
@@ -26,10 +33,47 @@ closeModal1.addEventListener("click", () => {
     modal1.close();
 });
 
+// Modal 2
 openModal2.addEventListener("click", () => {
     modal2.showModal();
 });
 
 closeModal2.addEventListener("click", () => {
     modal2.close();
+});
+
+// Modal 3
+openModal3.addEventListener("click", () => {
+    modal3.showModal();
+});
+
+closeModal3.addEventListener("click", () => {
+    modal3.close();
+});
+
+// Modal 4
+openModal4.addEventListener("click", () => {
+    modal4.showModal();
+});
+
+closeModal4.addEventListener("click", () => {
+    modal4.close();
+});
+
+// Modal 5
+openModal5.addEventListener("click", () => {
+    modal5.showModal();
+});
+
+closeModal5.addEventListener("click", () => {
+    modal5.close();
+});
+
+// Modal 6
+openModal6.addEventListener("click", () => {
+    modal6.showModal();
+});
+
+closeModal6.addEventListener("click", () => {
+    modal6.close();
 });
