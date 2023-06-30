11f791610409c8b49d7c314bc6be4cf5ec2b87b3
ImExtraPolite <jaleel1666@gmail.com>
2023-06-30 18:33:42 -0400

    make modal

--- javascript.js
+++ javascript.js
@@ -1,6 +1,21 @@
 const modal1 = document.querySelector(".modal1");
 const openModal1 = document.querySelector(".openModal1");
 const closeModal1 = document.querySelector(".closeModal1");
+const modal2 = document.querySelector(".modal2");
+const openModal2 = document.querySelector(".openModal2");
+const closeModal2 = document.querySelector(".closeModal2");
+const modal3 = document.querySelector(".modal3");
+const openModal3 = document.querySelector(".openModal3");
+const closeModal3 = document.querySelector(".closeModal3");
+const modal4 = document.querySelector(".modal4");
+const openModal4 = document.querySelector(".openModal4");
+const closeModal4 = document.querySelector(".closeModal4");
+const modal5 = document.querySelector(".modal5");
+const openModal5 = document.querySelector(".openModal5");
+const closeModal5 = document.querySelector(".closeModal5");
+const modal6 = document.querySelector(".modal6");
+const openModal6 = document.querySelector(".openModal6");
+const closeModal6 = document.querySelector(".closeModal6");
 
 
 openModal1.addEventListener("click", () => {
@@ -9,4 +24,12 @@ openModal1.addEventListener("click", () => {
 
 closeModal1.addEventListener("click", () => {
     modal1.close();
+});
+
+openModal2.addEventListener("click", () => {
+    modal2.showModal();
+});
+
+closeModal2.addEventListener("click", () => {
+    modal2.close();
 });
